81316861223c27fee600d943d7be14cfd2a79e92
Javier <javier_irigoyen96@hotmail.com>
2022-02-02 21:41:32 -0300

    pusheo del pi

--- api/src/db.js
+++ api/src/db.js
@@ -30,11 +30,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame, Genero } = sequelize.models;
+const { Videogame, Genre } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Videogame.belongsToMany(Genero, {through: "VideogameXGenero"});
+Videogame.belongsToMany(Genre, {through: "VideogameXGenero"});
 Genero.belongsToMany(Videogame, {through: "VideogameXGenero"});
 
 module.exports = {
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,25 +1,25 @@
 const { Router } = require('express');
 const axios = require ('axios');
-const { Videogame, Genre, VideogameXGender} = require('../db.js');
+const { Videogame, Genre, VideogameXGenre} = require('../db.js');
 const { Op } = require('sequelize');
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
+
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 const info = async ()=> {
     try{
     
-    let gamesPageOne = axios.get('https://api.rawg.io/api/games?key=eab826d4cf914afe83805016c7dd641d')
+    let gamesShowed = axios.get('https://api.rawg.io/api/games?key=6189995fbc584dedb6d4c7d34a1c5238')
    
-    let date = await Promise.all([gamesPageOne])
+    let date = await Promise.all([gamesShowed]);
 
-    gamesPageOne = date[0].data.results;
+    gamesShowed = date[0].data.results;
     
-    game = gamesPageOne;
+    let game = gamesShowed;
     
     game = game.map((result) => {
         return {
@@ -30,22 +30,21 @@ const info = async ()=> {
             image: result.background_image,
             rating: result.rating,
             platforms: result.platforms.map(e => e.platform.name),
-            genero: result.genero.map(e => e.name),
+            genres: result.genres.map(e => e.name),
         }        
     });
-     let juegos = []; 
+    let games = []; 
 
-    for(let i = 0; i < 100; i++){
-        juegos.push(game[i]);
+    for(let i = 0; i < 2; i++){
+        games.push(game[i]);
 
     }
-    return juegos;
+    return games;
     } catch (error){
         console.log(error);
     }
 
-
-}
+};
 
 const infoDB = async () => {
     const db = await Videogame.findAll({
@@ -56,97 +55,156 @@ const infoDB = async () => {
                 attributes: []
             }
         } 
-              
     });
     return db;
 };
+
 const apiInfoByName = async (name) => {
     let infoName = await axios.get(`https://api.rawg.io/api/games?search=${name}&key=6189995fbc584dedb6d4c7d34a1c5238`);
     infoName = infoName.data.results;
-   
+    
+
     let mapInfo = infoName.map(i => {
         return {
             id: i.id,
             name: i.name,
             description: i.description,
-            released:i.released,
+            released: i.released,
             image: i.background_image,
             rating: i.rating,
             platforms: i.platforms.map(e => e.platform.name),
-            genero: i.genero.map(e => e.name),
-    };
-})
+            genres: i.genres.map(e => e.name),
+        }
+    });
     
-        return mapInfo
-    };
+    return mapInfo;
+}
+const apiInfoByid = async (id) => {
+    let infoId = await axios.get(`https://api.rawg.io/api/games/${id}?key=6189995fbc584dedb6d4c7d34a1c5238`);
+    let infoIds = infoId.data;
 
-    const apiInfoById = async (id) => {
-        let infoId = await axios.get(`https://api.rawg.io/api/games/${id}&key=eab826d4cf914afe83805016c7dd641`)
-    }
-router.get('/database', async (req, res) => {s
+    
+        return {
+            id: infoIds.id,
+            name: infoIds.name,
+            description: infoIds.description,
+            released: infoIds.released,
+            image: infoIds.background_image,
+            rating: infoIds.rating,
+            platforms: infoIds.platforms.map(e => e.platform.name),
+            genres: infoIds.genres.map(e => e.name),
+        }
+    
+        
+}
+
+
+router.get('/database', async (req, res) => {
     try {
         const dbVideogame = await infoDB();
         res.send(dbVideogame);
     } catch (error) {
-        res.status(400).send('not found', error);
+        res.status(400).send(error);
     }
         
 })
+
+
+router.get('/', async (req, res) => {
+    const apiVideogame = await info();
+
+    const dbVideogame = await infoDB();
+
+    res.send(dbVideogame.concat(apiVideogame));
+
+    
+});
 
 router.get('/videogames', async (req, res) => {
-    const name = req.query.name; 
-    const apiVideogame = await info();
-    const dbVideogame = await infoDB();
-    res.send(dbVideogame.concat(apiVideogame)); 
-
-    router.get('/', async (req, res) => {
-        const names= req.query.name;
-
-        if(name) {
-            try {
-                const apiGames = await apiInfoByName(name);
-                 const dbGames = await Videogame.findAll({
-                    where: {
-                        name: {
-                            [Op.iLike]: '%' + name + '%'
-                        }
-                    },
-                    include: [{
-                        model:  Genre
-                    }]
-                    
-                });
-                const totalGames = dbGames.concat(apiGames)
-                return res.json(totalGames);
-            } catch (error) {
+    const name = req.query.name;
+    
+    if(name) {
+        try {
+        const apiGames = await apiInfoByName(name);
+        const dbGames = await Videogame.findAll({
+                where: {
+                    name: {
+                        [Op.iLike]: '%' + name + '%'
+                    }
+                },
+                include: [{
+                    model:  Genre
+                }]
+                
+            });
+            const totalGames = dbGames.concat(apiGames)
+            return res.json(totalGames);
+        } catch (error) {
             console.log(error)
-            }
-        
-   
-           
-router.post('/', async(req, res) => {
-    const { name, description, released, rating, platforms, image } = req.body;
+        }
+    } 
+});
+
+router.get('/videogame/:id', async (req, res) => {
+    const id = req.params.id;
     try{
-        if(name && description && platforms) {
-         await Videogame.create({
-            name,
-            description,
-            released,
-            rating,
-            platforms,
-            image
-        })
-        dbGameCreate.addGenre(genreDB)
-        return res.send('se creo juego');
-    }
-}}catch(error) {
-    res.status(400).send({message:"error"})
+        const dbGames = await Videogame.findByPk(id,{
+                
+                include: {
+                    model:  Genre,
+                    attributes: ['name'],
+                through:{
+                attributes: []
+            }
+                }
+                
+            });
+            if(dbGames){
+                res.json(dbGames)
+            }
+            
+        }catch(error){
+            try {
+                apiGames = await apiInfoByid(id);
+                res.json(apiGames)
+                
+                /* const totalGames = dbGames.concat(apiGames)
+            res.json(totalGames); */
+        } catch (error) {
+            console.log(error)
+        }}
+     
+})
+
+router.post('/', async(req, res) => {
+    const { name, description, released, rating, platforms, image, genre } = req.body;
+    try{
+        let genreDB = await Genre.findAll({
+            where: { name: genre },
+        });
+
+            if(name && description && platforms) {
+            let dbGameCreate = await Videogame.create({
+                name,
+                description,
+                released,
+                rating,
+                platforms,
+                image
+            })
+
+            dbGameCreate.addGenre(genreDB);
+            return res.send('Your game has been succesfully created');
+           // }
+        }}catch(error) {
+        res.status(400).send({message:'Error'})
         console.log(error)
     }
-})
+});
+
 router.get('/genre', async(req, res) => {
     try{
-    let genre = await axios.get('https://api.rawg.io/api/genres?key=6189995fbc584dedb6d4c7d34a1c5238');
+    let genre = await axios.get('https://api.rawg.io/api/genres?key=eab826d4cf914afe83805016c7dd641d');
     genre = genre.data.results;
     
     let mapGenre = genre.map(g => {
@@ -172,5 +230,6 @@ router.get('/genre', async(req, res) => {
     }
     
 })
+
 
 module.exports = router;
